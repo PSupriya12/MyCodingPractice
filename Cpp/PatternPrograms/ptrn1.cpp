a8eb7f345203d547cc24e4f1162ae429653fd98f
Supriya Pawar <90033745+PSupriya12@users.noreply.github.com>
2023-01-03 22:34:17 +0530

    Update ptrn1.cpp

--- Cpp/PatternPrograms/ptrn1.cpp
+++ Cpp/PatternPrograms/ptrn1.cpp
@@ -1,1 +1,19 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i=1,j;
+    cout<<"Enter number:";
+    cin>>n;
+    while(i<=n)
+    {
+        j=1;
+        while(j<=n)
+        {
+            cout<<j;
+            j++;
+        }
+        cout<<"\n";
+        i++;
+    }
+}
