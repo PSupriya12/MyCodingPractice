8277169007005533dc34e53af7c42e3e7a3d5baa
Supriya Pawar <90033745+PSupriya12@users.noreply.github.com>
2023-05-13 17:52:54 +0530

    Create Memorize.js

---
+++ DailyLeetcode/30 Days of LC JavaScript Challenge/Memorize.js
@@ -0,0 +1,31 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+
+    const cache = {};
+
+    return function(...args) {
+        if(args in cache)
+        {
+            return cache[args];
+        }
+
+        const result = fn.apply(this,args);
+        cache[args] = result;
+        return result;
+
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
